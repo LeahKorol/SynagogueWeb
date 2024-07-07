187a44a7e6f52be041a66b6bae6863493bdb0fee
Talya <talya8399@gmail.com>
2024-07-07 16:09:12 +0300

    add hero section - until without image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import logo from './logo.svg';
 import './App.css';
 import './styles/try.css';
 import Header from './components/Header';
@@ -12,6 +11,20 @@ function App() {
         <title>פאתי מזרח | דף בית</title>
       </Helmet>
       <Header />
+      <section id="hero" className="hero">
+        <div className="content">
+          <h1>"שויתי ה' לנגדי תמיד"</h1>
+          <p>
+            ברוכים הבאים לאתר של קהילת פאתי מזרח.
+            <br />
+            כאן תמצאו מידע על זמני תפילות, אירועים קרובים ושיעורי תורה
+          </p>
+
+          <div className="buttons">
+            <a href="about-contact.html">עוד קצת עלינו</a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
