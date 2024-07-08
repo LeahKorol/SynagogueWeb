17f6196e0f777a9552e296074093135ea5157784
Leah Korol <leah03120@gmail.com>
2024-07-08 16:58:28 +0300

    update CongratForm

--- src/components/CongratForm.js
+++ src/components/CongratForm.js
@@ -1,38 +1,47 @@
 import React, { useState } from 'react'
 
 import db from '../firebase';
-
-const [congrat, setCongrat] = useState({ title: "", content: "" });
-
-const updateForm = (e) => {
-    e.preventDefault();
-    setCongrat({ ...congrat, [e.target.name]: e.target.value });
-    console.log("update", e.target.name, e.target.value);
-};
-
-const submitCongrat = (e) => {
-    e.preventDefault();
-    console.log("Saving :", congrat)
-}
-
 function CongratForm() {
+
+    const [congrat, setCongrat] = useState({ title: "", content: "" });
+
+    const updateForm = (e) => {
+        e.preventDefault();
+        setCongrat({ ...congrat, [e.target.name]: e.target.value });
+        console.log("update", e.target.name, e.target.value);
+    };
+
+    const submitCongrat = (e) => {
+        e.preventDefault();
+        console.log("Saving :", congrat)
+    }
+
+
 
     return (
         <form onSubmit={submitCongrat}>
+        <label>
+            Title
             <input
                 name="title"
                 value={congrat.title}
                 type="text"
                 onChange={updateForm}
-            >Title</input>
+            />
+        </label>
+        <label>
+            Content
             <input
                 name="content"
                 value={congrat.content}
                 type="text"
                 onChange={updateForm}
-            >Content</input>
-            <button type="submit">Save</button>
-        </form>
+            />
+        </label>
+        <button type="submit">Save</button>
+    </form>
 
-    )
+    );
 }
+
+export default CongratForm;
--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import CongratForm from './CongratForm';
+
 
 function Manager() {
   return (
     <div className="manager-container">
       <h2>עמוד מנהל</h2>
       <p>ברוך הבא למערכת המנהל.</p>
+      <CongratForm/>
     </div>
   );
 }
