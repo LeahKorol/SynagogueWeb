6c12d2645c2771e566bf22a3363e48ce59d707ad
Leah Korol <leah03120@gmail.com>
2024-07-09 00:09:25 +0300

    Create HomePage component and refactor homepage content
    - Created a new component, `HomePage`, to encapsulate the structure and content specific to the homepage.
    - Moved homepage content, including header, hero section, prayer times display, lessons activities, announcements, and map embed, into the `HomePage` component.
    - Updated `App.js` to render `HomePage` component for the root route (`/`).

--- src/App.js
+++ src/App.js
@@ -2,36 +2,11 @@ import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-
-import Hero from './components/Hero';
-import PrayerTimes from './components/PrayerTimes';
-import LessonsActivities from './components/LessonsActivities';
-import Announcements from './components/Announcements';
-import MapEmbed from './components/MapEmbed';
-
+import HomePage from './components/HomePage';
 import AdminLogin from './components/AdminLogin';
 import Manager from './components/Manager';
 
 function App() {
-  // זמני תפילות ליום חול
-  const weekdayTimes = {
-    shacharit: "06:15",
-    mincha_gedola: "13:15",
-    mincha: "19:33",
-    arvit_1: "20:08",
-    arvit_2: "21:50",
-  };
-
-  // זמני תפילות לשבת
-  const shabbatTimes = {
-    mincha_erev_shabbat: "19:08",
-    arvit_shabbat: "19:38",
-    shacharit_shabbat: "08:30",
-    mincha_gedola_shabbat: "12:30",
-    mincha_ketana_shabbat: "19:10",
-  };
-
   return (
     <HelmetProvider>
       <Router>
@@ -40,20 +15,9 @@ function App() {
             <title>פאתי מזרח | דף בית</title>
           </Helmet>
           <Routes>
-
-            <Route path="/" element={ //homepage
-              <main>
-                <Header />
-                <Hero />
-                <PrayerTimes weekdayTimes={weekdayTimes} shabbatTimes={shabbatTimes} />
-                <LessonsActivities />
-                <Announcements />
-                <MapEmbed />
-              </main>
-            } />
-
-              <Route path="/admin-login" element={<AdminLogin />} />
-              <Route path="/manager" element={<Manager />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/admin-login" element={<AdminLogin />} />
+            <Route path="/manager" element={<Manager />} />
           </Routes>
         </div>
       </Router>
@@ -62,4 +26,3 @@ function App() {
 }
 
 export default App;
-
