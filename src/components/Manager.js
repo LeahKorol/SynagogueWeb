d943a8691171be030db800206b15bb1f9ddfb769
Shilat Tzur <shilattzur10@gmail.com>
2024-07-11 11:58:00 +0300

    contactForm

--- src/components/Manager.js
+++ src/components/Manager.js
@@ -1,26 +1,10 @@
-// import React from 'react';
-// import CongratForm from './CongratForm';
-// import Congrats from './Congrats';
 
-
-// function Manager() {
-//   return (
-//     <div className="manager-container">
-//       <h2>עמוד מנהל</h2>
-//       <p>ברוך הבא למערכת המנהל.</p>
-//       <CongratForm/>
-//       <Congrats/>
-//     </div>
-//   );
-// }
-
-// export default Manager;
 import React from 'react';
 import CongratForm from './CongratForm';
 import LessonsForm from './LessonsForm';
 import EventGalleryForm from './EventGalleryForm';
-// import ImageUploader from './EventHall/ImageUploader';
-// import Gallery from './EventHall/Gallery';
+import ContactForm from './ContactForm'; // ייבוא טופס ניהול צור הקשר
+
 
 function Manager() {
   return (
@@ -30,8 +14,7 @@ function Manager() {
       <CongratForm />
       <LessonsForm />
       <EventGalleryForm />
-      {/* <Gallery mode="admin" />
-      <ImageUploader/> */}
+      <ContactForm /> 
     </div>
   );
 }
