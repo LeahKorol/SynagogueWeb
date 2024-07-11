d9da4b94e5e73f2bdcf60036122203308512c99b
Talya <talya8399@gmail.com>
2024-07-11 21:35:00 +0300

    collapsible section design

--- src/components/Admin/Manager.js
+++ src/components/Admin/Manager.js
@@ -1,26 +1,42 @@
 import React from 'react';
+import CollapsibleSection from './Forms/CollapsibleSection';
 import CongratForm from './Forms/CongratForm';
 import LessonsForm from './Forms/LessonsForm';
 import EventGalleryForm from './Forms/EventGalleryForm';
-import ContactForm from './Forms/ContactForm'; 
+import ContactForm from './Forms/ContactForm';
 import AboutForm from './Forms/AboutForm';
 import ContributesForm from './Forms/ContributesForm';
+import './Manager.css';
 
 function Manager() {
   return (
     <div className="manager-container">
-      <h2>עמוד מנהל</h2>
-      <p>ברוך הבא למערכת המנהל.</p>
-      <CongratForm />
-      <LessonsForm />
-      <EventGalleryForm />
-      <ContactForm /> 
-      <AboutForm /> 
-      <ContributesForm />
+      <header className="manager-header">
+        <h2>עמוד מנהל</h2>
+        <p>ברוך הבא למערכת המנהל</p>
+      </header>
+      <main className="manager-content">
+        <CollapsibleSection title="עדכון איחולים">
+          <CongratForm />
+        </CollapsibleSection>
+        <CollapsibleSection title="ניהול שיעורים">
+          <LessonsForm />
+        </CollapsibleSection>
+        <CollapsibleSection title="גלריית אירועים">
+          <EventGalleryForm />
+        </CollapsibleSection>
+        <CollapsibleSection title="פרטי התקשרות">
+          <ContactForm />
+        </CollapsibleSection>
+        <CollapsibleSection title="עדכון אודות הקהילה">
+          <AboutForm />
+        </CollapsibleSection>
+        <CollapsibleSection title="ניהול תרומות">
+          <ContributesForm />
+        </CollapsibleSection>
+      </main>
     </div>
   );
 }
 
 export default Manager;
-
-
