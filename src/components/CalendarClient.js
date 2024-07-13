5de22ea04bfc0fa8b36580f5feae8c20c01b94c5
avigailnaveh <avigail.naveh@gmail.com>
2024-07-13 23:45:59 +0300

    הוספתי דפים למשתמש קיצר מה שבתוך הmain

---
+++ src/components/CalendarClient.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from 'react';
+import { getDocs, collection } from 'firebase/firestore';
+import { db } from '../firebase';
+import { getEventsCalendar, getHebrewDate } from '../utils/calendar';
+import { getCurrentGregJerusalemDate } from '../utils/JerusalemDate';
+import Month from './MonthClient';
+import '../styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faChevronLeft, faChevronRight, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
+
+const CalendarClient = () => {
+    const [events, setEvents] = useState([]);
+    const [currentMonth, setCurrentMonth] = useState(getCurrentGregJerusalemDate());
+    const [selectedDay, setSelectedDay] = useState(null);
+
+    const currentYear = currentMonth.getFullYear();
+    const months = [...Array(12).keys()].map(i => new Date(currentYear, i, 1));
+
+    const fetchEvents = async () => {
+        try {
+            const eventsList = [];
+            const querySnapshot = await getDocs(collection(db, "events"));
+            querySnapshot.forEach(doc => {
+                eventsList.push({ id: doc.id, ...doc.data() });
+            });
+            const eventsCalendar = await getEventsCalendar();
+            setEvents([...eventsList, ...eventsCalendar]);
+        } catch (error) {
+            console.error("Error fetching events: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const handleEventChange = () => {
+        fetchEvents();
+    };
+
+    const prevMonth = () => {
+        setCurrentMonth(prevMonth => new Date(prevMonth.getFullYear(), prevMonth.getMonth() - 1, 1));
+    };
+
+    const nextMonth = () => {
+        setCurrentMonth(prevMonth => new Date(prevMonth.getFullYear(), prevMonth.getMonth() + 1, 1));
+    };
+
+    const handleDayClick = (day) => {
+        setSelectedDay(day);
+    };
+
+    const goToToday = () => {
+        setCurrentMonth(getCurrentGregJerusalemDate());
+    };
+    
+    const getMonthTitle = (month) => {
+        const gregorianDate = new Date(month.getFullYear(), month.getMonth(), 1);
+        const hebrewStartDate = getHebrewDate(gregorianDate);
+        const hebrewEndDate = getHebrewDate(new Date(month.getFullYear(), month.getMonth() + 1, 0));
+    
+        const hebrewStartMonth = hebrewStartDate.split(' ')[1];
+        const hebrewEndMonth = hebrewEndDate.split(' ')[1];
+        const hebrewYear = hebrewStartDate.split(' ')[2];
+        
+      
+        if(hebrewStartDate === hebrewEndDate){
+            if( hebrewStartDate.split(' ')[2]=== "א׳"){
+                return ` ${hebrewStartDate.split(' ')[1]} ${hebrewStartDate.split(' ')[2]} `;
+            }else if(hebrewStartDate.split(' ')[2]==="ב׳"){
+                return ` ${hebrewStartDate.split(' ')[1]} ${hebrewStartDate.split(' ')[2]} `;
+            }else {
+                return ` ${hebrewStartMonth} ${hebrewYear}`;
+            }
+        }else{
+            if( hebrewStartDate.split(' ')[2]=== "א׳"){
+                return ` ${hebrewStartDate.split(' ')[1]} ${hebrewStartDate.split(' ')[2]} - ${hebrewEndDate.split(' ')[1]} ${hebrewEndDate.split(' ')[2]} ${hebrewEndDate.split(' ')[3]}`;
+            }else if( hebrewEndDate.split(' ')[2]==="א׳"){
+                return ` ${hebrewStartDate.split(' ')[1]} - ${hebrewEndDate.split(' ')[1]} ${hebrewEndDate.split(' ')[2]} ${hebrewEndDate.split(' ')[3]}`;
+            }else if(hebrewStartDate.split(' ')[2]==="ב׳"){
+                return ` ${hebrewStartDate.split(' ')[1]} ${hebrewStartDate.split(' ')[2]} - ${hebrewEndDate.split(' ')[1]} ${hebrewEndDate.split(' ')[2]}`;
+            }else {
+                return ` ${hebrewStartMonth} - ${hebrewEndMonth} ${hebrewYear}`;      
+            }
+        } 
+    };
+    const getYearTitle=(month)=>{
+        const gregorianMonth = month.toLocaleString('he-IL', { month: 'long' });
+        const gregorianYear = month.getFullYear();
+        return `${gregorianMonth} ${gregorianYear}`;
+    }
+    const filteredMonths = months.filter(month => month.getMonth() === currentMonth.getMonth());
+
+    return (
+        <div className="calendarCenter">
+            <div className="calendar-container">
+                <div className="calendar-header"> 
+                    <div className="controls">
+                        <button className="today-button" onClick={goToToday}>
+                            <FontAwesomeIcon icon={faCalendarDay} />
+                            <span>היום</span>
+                        </button>
+                        <button className="arrow-button" onClick={nextMonth}>
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </button>
+                        <button className="arrow-button" onClick={prevMonth}>
+                            <FontAwesomeIcon icon={faChevronRight} />
+                        </button>
+                    </div>
+                    <div className='title'>
+                        <h2 className="month-title month-year">
+                            {getYearTitle(currentMonth)}
+                        </h2>
+                        <h2 className="month-title">
+                            {getMonthTitle(currentMonth)}
+                        </h2>
+                    </div>
+                </div>
+                <div className="calendar">
+                    {filteredMonths.map((month, index) => (
+                        <Month 
+                            key={index} 
+                            month={month} 
+                            events={events} 
+                            onEventChange={handleEventChange}
+                            onDayClick={handleDayClick}
+                            selectedDay={selectedDay}
+                        />
+                    ))}
+                </div>
+            </div>
+            
+           
+        </div>
+    );
+};
+
+export default CalendarClient;
