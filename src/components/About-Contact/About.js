bc4f839ea6c2479ac50221782c00eba40910de15
Talya <talya8399@gmail.com>
2024-07-15 09:36:09 +0300

    addcredit for hebcal and add link to thier website

--- src/components/About-Contact/About.js
+++ src/components/About-Contact/About.js
@@ -1,28 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
+import React from 'react';
 import './About.css';
 import aboutImage from '../../images/1.jfif';
 
 function AboutUs() {
-  const [aboutData, setAboutData] = useState({});
-
-  useEffect(() => {
-    const fetchAboutData = async () => {
-      try {
-        const docRef = doc(db, 'about', 'about-data');
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setAboutData(docSnap.data());
-        }
-      } catch (error) {
-        console.error('Error fetching document: ', error);
-      }
-    };
-
-    fetchAboutData();
-  }, []);
-
   return (
     <main className='about-section'>
       <h1 className="heading-about">מי אנחנו?</h1>
@@ -36,20 +16,13 @@ function AboutUs() {
             לכל חבריה גברים, נשים וטף לפעילות תורנית, חינוכית, תרבותית וחברתית בכל ימות השנה, ומקור של אור
             וחסד המקרין על חברי הקהילה ועל כלל תושבי השכונה.
           </p>
-          <p>
-            {aboutData.about || ''}
-          </p>
           <div className="files">
-            {aboutData.friendsList && (
-              <a className="more-details view-button" href={aboutData.friendsList} target="_blank" rel="noopener noreferrer">
-                לרשימת החברים לחץ כאן
-              </a>
-            )}
-            {aboutData.rules && (
-              <a className="more-details view-button" href={aboutData.rules} target="_blank" rel="noopener noreferrer">
-                לתקנון האתר לחץ כאן
-              </a>
-            )}
+            <a className="more-details view-button" href="LINK_TO_FRIENDS_LIST" target="_blank" rel="noopener noreferrer">
+              לרשימת החברים לחץ כאן
+            </a>
+            <a className="more-details view-button" href="LINK_TO_RULES" target="_blank" rel="noopener noreferrer">
+              לתקנון האתר לחץ כאן
+            </a>
           </div>
         </div>
       </div>
