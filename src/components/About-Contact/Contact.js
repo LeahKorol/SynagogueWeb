aba57c0f233527757e5561af842dd39691e12c44
Shilat Tzur <shilattzur10@gmail.com>
2024-07-17 16:21:15 +0300

    tenporary commit

--- src/components/About-Contact/Contact.js
+++ src/components/About-Contact/Contact.js
@@ -1,3 +1,56 @@
+// import React, { useEffect, useState } from 'react';
+// import { collection, getDocs } from 'firebase/firestore';
+// import { db } from '../../firebase';
+// import './Contact.css';
+
+// function Contact() {
+//   const [contactDetails, setContactDetails] = useState({});
+
+//   const fetchContactDetails = async () => {
+//     try {
+//       const querySnapshot = await getDocs(collection(db, "contact"));
+//       if (!querySnapshot.empty) {
+//         const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))[0];
+//         setContactDetails(data);
+//       }
+//     } catch (e) {
+//       console.error("Error fetching contact details: ", e);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchContactDetails();
+//   }, []);
+
+//   return (
+//     <div id="contact" className="contact-div">
+//       <h1 className="heading-contact">צור קשר</h1>
+//       <div className="contact-details">
+//         <div className="box">
+//           <i className="fas fa-map-marker-alt"></i>
+//           <h3 className="header-box">כתובת</h3>
+//           <p className="detail">
+//             {contactDetails.address}
+//           </p>
+//         </div>
+//         <div className="box">
+//           <i className="fas fa-envelope-open"></i>
+//           <h3 className="header-box">מייל</h3>
+//           <p className="detail">{contactDetails.email}</p>
+//         </div>
+//         <div className="box">
+//           <i className="fas fa-phone"></i>
+//           <h3 className="header-box">טלפון</h3>
+//           <p className="detail-phone">
+//             {contactDetails.phone}
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Contact;
 import React, { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase';
@@ -42,7 +95,7 @@ function Contact() {
           <i className="fas fa-phone"></i>
           <h3 className="header-box">טלפון</h3>
           <p className="detail-phone">
-            {contactDetails.phone}
+            {contactDetails.name ? `${contactDetails.name} - ` : ''}{contactDetails.phone}
           </p>
         </div>
       </div>
--- src/components/Admin/Forms/ContactForm.js
+++ src/components/Admin/Forms/ContactForm.js
@@ -1,10 +1,136 @@
+// import React, { useState, useEffect, useRef } from 'react';
+// import { addDoc, collection, updateDoc, doc, getDocs } from 'firebase/firestore';
+// import { db } from '../../../firebase';
+
+// function ContactForm() {
+//   const [contactDetails, setContactDetails] = useState({ address: '', email: '', phone: '' });
+//   const [editContact, setEditContact] = useState({ id: '', address: '', email: '', phone: '' });
+//   const [originalDetails, setOriginalDetails] = useState({});
+//   const formRef = useRef(null);
+
+//   const fetchContactDetails = async () => {
+//     try {
+//       const querySnapshot = await getDocs(collection(db, "contact"));
+//       if (!querySnapshot.empty) {
+//         const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))[0];
+//         setContactDetails(data);
+//         setOriginalDetails(data);
+//       }
+//     } catch (e) {
+//       console.error("Error fetching contact details: ", e);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchContactDetails();
+
+//     const handleClickOutside = (event) => {
+//       if (formRef.current && !formRef.current.contains(event.target) && editContact.id) {
+//         setContactDetails(originalDetails);
+//         setEditContact({ id: '', address: '', email: '', phone: '' });
+//       }
+//     };
+
+//     document.addEventListener("mousedown", handleClickOutside);
+//     return () => {
+//       document.removeEventListener("mousedown", handleClickOutside);
+//     };
+//   }, [editContact, originalDetails]);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setEditContact((prevDetails) => ({
+//       ...prevDetails,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleEditFocus = () => {
+//     setEditContact({ ...contactDetails, id: contactDetails.id || '' });
+//     setOriginalDetails(contactDetails);
+//   };
+
+//   const handleUpdate = async () => {
+//     if (!editContact.address || !editContact.email || !editContact.phone) {
+//       alert('נא למלא את כל השדות');
+//       return;
+//     }
+
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (!emailRegex.test(editContact.email)) {
+//       alert('נא להזין כתובת אימייל תקינה');
+//       return;
+//     }
+
+//     const isConfirmed = window.confirm("האם אתה בטוח שברצונך לשמור את השינויים?");
+//     if (isConfirmed) {
+//       try {
+//         if (editContact.id) {
+//           const docRef = doc(db, "contact", editContact.id);
+//           await updateDoc(docRef, editContact);
+//         } else {
+//           await addDoc(collection(db, "contact"), editContact);
+//         }
+//         fetchContactDetails();
+//         setEditContact({ id: '', address: '', email: '', phone: '' });
+//       } catch (e) {
+//         console.error("Error saving contact details: ", e);
+//       }
+//     } else {
+//       setEditContact({ ...originalDetails, id: originalDetails.id || '' });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h2>ניהול פרטי צור קשר</h2>
+//       <div ref={formRef}>
+//         <label>
+//           כתובת:
+//           <input
+//             type="text"
+//             name="address"
+//             value={editContact.id ? editContact.address : contactDetails.address}
+//             onChange={handleChange}
+//             onFocus={handleEditFocus}
+//             required
+//           />
+//         </label>
+//         <label>
+//           מייל:
+//           <input
+//             type="email"
+//             name="email"
+//             value={editContact.id ? editContact.email : contactDetails.email}
+//             onChange={handleChange}
+//             onFocus={handleEditFocus}
+//             required
+//           />
+//         </label>
+//         <label>
+//           טלפון:
+//           <input
+//             type="text"
+//             name="phone"
+//             value={editContact.id ? editContact.phone : contactDetails.phone}
+//             onChange={handleChange}
+//             onFocus={handleEditFocus}
+//             required
+//           />
+//         </label>
+//         <button onClick={handleUpdate}>עדכן</button>
+//       </div>
+//     </div>
+//   );
+// }
+// export default ContactForm;
 import React, { useState, useEffect, useRef } from 'react';
 import { addDoc, collection, updateDoc, doc, getDocs } from 'firebase/firestore';
 import { db } from '../../../firebase';
 
 function ContactForm() {
-  const [contactDetails, setContactDetails] = useState({ address: '', email: '', phone: '' });
-  const [editContact, setEditContact] = useState({ id: '', address: '', email: '', phone: '' });
+  const [contactDetails, setContactDetails] = useState({ address: '', email: '', name: '', phone: '' });
+  const [editContact, setEditContact] = useState({ id: '', address: '', email: '', name: '', phone: '' });
   const [originalDetails, setOriginalDetails] = useState({});
   const formRef = useRef(null);
 
@@ -27,7 +153,7 @@ function ContactForm() {
     const handleClickOutside = (event) => {
       if (formRef.current && !formRef.current.contains(event.target) && editContact.id) {
         setContactDetails(originalDetails);
-        setEditContact({ id: '', address: '', email: '', phone: '' });
+        setEditContact({ id: '', address: '', email: '', name: '', phone: '' });
       }
     };
 
@@ -51,7 +177,7 @@ function ContactForm() {
   };
 
   const handleUpdate = async () => {
-    if (!editContact.address || !editContact.email || !editContact.phone) {
+    if (!editContact.address || !editContact.email || !editContact.name || !editContact.phone) {
       alert('נא למלא את כל השדות');
       return;
     }
@@ -72,7 +198,7 @@ function ContactForm() {
           await addDoc(collection(db, "contact"), editContact);
         }
         fetchContactDetails();
-        setEditContact({ id: '', address: '', email: '', phone: '' });
+        setEditContact({ id: '', address: '', email: '', name: '', phone: '' });
       } catch (e) {
         console.error("Error saving contact details: ", e);
       }
@@ -102,6 +228,17 @@ function ContactForm() {
             type="email"
             name="email"
             value={editContact.id ? editContact.email : contactDetails.email}
+            onChange={handleChange}
+            onFocus={handleEditFocus}
+            required
+          />
+        </label>
+        <label>
+          שם:
+          <input
+            type="text"
+            name="name"
+            value={editContact.id ? editContact.name : contactDetails.name}
             onChange={handleChange}
             onFocus={handleEditFocus}
             required
